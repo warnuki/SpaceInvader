c169d558d9560f733eebfb14addc0e3848473830
warnuki <60508768+warnuki@users.noreply.github.com>
2021-12-17 08:50:36 +0100

    Fresh start

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+//importation
+
+//Création de l'application pixi
+const app = new PIXI.Application({
+    width : 1000,
+    height : 500,
+    backgroundColor : 0x33333,
+    antialias : true
+});
+
+document.body.appendChild(app.view);
